3ad0587cf970e1c007ea10de98e90a87f640a278
OmarGhoz <omarghoz999@gmail.com>
2024-11-23 23:17:01 +0200

    permission_classes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics , viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.viewsets import ModelViewSet  # Ensure this is correctly imported
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import BasePermission
 
 # List view for books
 class BookList(generics.ListAPIView):
@@ -16,3 +18,13 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerOrReadOnly]
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions are allowed for any request
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        # Write permissions are only allowed to the owner of the object
+        return obj.owner == request.user
