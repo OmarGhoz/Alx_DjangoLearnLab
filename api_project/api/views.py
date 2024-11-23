ba5cb79fc67850ebe51d57e48bcf13858db7f5cf
OmarGhoz <omarghoz999@gmail.com>
2024-11-23 22:44:13 +0200

    resolve issue

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,10 @@
 # Using ListAPIView for BookList
 from rest_framework.generics import ListAPIView
-
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
