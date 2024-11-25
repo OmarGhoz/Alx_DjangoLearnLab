ac0f0f0f6811a80761ba0c99188533b50862f28d
OmarGhoz <omarghoz999@gmail.com>
2024-11-25 18:22:38 +0200

    Correct URL Configuration for Update and Delete

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from api.views import BookUpdateView, BookDeleteView
 
-# Create your tests here.
+class URLConfigTest(TestCase):
+    def test_update_url(self):
+        url = reverse('book-update', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).func.view_class, BookUpdateView)
+
+    def test_delete_url(self):
+        url = reverse('book-delete', kwargs={'pk': 1})
+        self.assertEqual(resolve(url).func.view_class, BookDeleteView)
+
